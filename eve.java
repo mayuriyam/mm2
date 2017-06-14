549d3f713cf209311bd0cf965568cfb9dcfc95be
mayuriyam <mayuriya21@gmail.com>
2017-06-14 18:37:51 +0530

    Create eve.java

---
+++ eve.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class eve
+{
+    public static void main(String ar[])
+    {
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+        
+        if(n%2==0)
+        {
+            System.out.println("even");
+        }
+        else 
+        {
+            System.out.println("odd");
+        }
+      
+        
+    }
+}
